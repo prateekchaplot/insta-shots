5ebdfb2b0aecdc4ed8896dfcad222b2e2f9c67db
Prateek Chaplot <prateekchaplot96@gmail.com>
2023-07-08 23:01:40 +0530

    add api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import Videos from "./dbModel.js";
+import Cors from "cors";
 
 // App config
 const app = express();
@@ -14,6 +16,22 @@ mongoose.connect(connection_url, {
   useUnifiedTopology: true,
 });
 
+app.use(express.json());
+app.use(Cors());
+
 app.get("/", (req, res) => res.status(200).send("Hello world"));
+
+app.post("/v2/posts", (req, res) => {
+  const videos = req.body;
+  Videos.insertMany(videos)
+    .then((data) => res.status(201).send(data))
+    .catch((err) => res.status(500).send(err));
+});
+
+app.get("/v2/posts", (req, res) => {
+  Videos.find()
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(500).send(err));
+});
 
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
